5a4e63f511610929bb7aa48d79c237ea6f106aba
QuocHuy2811 <0306231201@caothang.edu.vn>
2025-10-20 09:52:54 +0700

    Xoa tat ca file tren branch master

--- resources/views/admin/blankpageadmin.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.admin')
-@section('title','Blank Page')
-@section('css')
-
-@section('js')
-
-@section('content')
-    <p style="font-size: 50px; color: grey;" class="text-center">Welcome to Blank Page</p>
-@endsection
