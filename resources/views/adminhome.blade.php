8b7c78de3cd4cbeb07ad19471f7efdc158fe6292
QuocHuy2811 <0306231201@caothang.edu.vn>
2025-10-20 10:17:36 +0700

    update du an nhanh MinhHau

--- resources/views/adminhome.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.admin')
-@section('title','Admin Home')
-@section('content')
-    <p style="font-size: 50px; color: grey;" class="text-center">Chào mừng đến với trang Admin</p>
-@endsection
--- resources/views/blankpageadmin.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.admin')
-@section('title','Blank Page')
-@section('content')
-    <p style="font-size: 50px; color: grey;" class="text-center">Welcome to Blank Page</p>
-
-@endsection
